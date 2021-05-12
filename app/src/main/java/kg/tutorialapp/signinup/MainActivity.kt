4dd528fd5e3090a9c22ff09c9ac9f38d16b7c720
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-13 02:20:47 +0600

    setup sign up

--- app/src/main/java/kg/tutorialapp/signinup/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/signinup/MainActivity.kt
@@ -1,7 +1,9 @@
 package kg.tutorialapp.signinup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kg.tutorialapp.signinup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,4 +22,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constance.REQUEST_CODE_SIGN_UP){
+
+            login = data?.getStringExtra(Constance.LOGIN)!!
+            password = data?.getStringExtra(Constance.PASSWORD)!!
+            name = data?.getStringExtra(Constance.NAME)!!
+            firstName = data?.getStringExtra(Constance.FIRST_NAME)!!
+            secondName = data?.getStringExtra(Constance.SECOND_NAME)!!
+
+            bindingClass.imAvatar.visibility = View.VISIBLE
+            avatarImageId = data?.getIntExtra(Constance.AVATAR_ID, 0)
+
+            val textInfo = "$name $firstName $secondName"
+            bindingClass.tvInfo.text = textInfo
+
+            bindingClass.bHide.visibility = View.GONE
+            bindingClass.bExit.text = "Exit"
+
+        }
+    }
+
+    fun onClickSignUp(v: View){
+        val intent = Intent(this, SignInUpAct::class.java)
+        intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_UP_STATE)
+        startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_UP)
+
+    }
 }
--- app/src/main/java/kg/tutorialapp/signinup/SignInUpAct.kt
+++ app/src/main/java/kg/tutorialapp/signinup/SignInUpAct.kt
@@ -1,4 +1,40 @@
 package kg.tutorialapp.signinup
 
-class SignInUpAct {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import kg.tutorialapp.signinup.databinding.SignInUpBinding
+
+class SignInUpAct: AppCompatActivity() {
+
+    lateinit var bindingClass: SignInUpBinding
+    private var signState = "empty"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = SignInUpBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        signState = intent.getStringExtra(Constance.SIGN_STATE)!!
+
+    }
+
+    fun onClickDone(v: View) {
+
+        if (signState == Constance.SIGN_UP_STATE) {
+
+            val intent = Intent()
+            intent.putExtra(Constance.LOGIN, bindingClass.edLogin.text.toString())
+            intent.putExtra(Constance.PASSWORD, bindingClass.edPassword.text.toString())
+            intent.putExtra(Constance.NAME, bindingClass.edName.text.toString())
+            intent.putExtra(Constance.FIRST_NAME, bindingClass.edFirstName.text.toString())
+            intent.putExtra(Constance.SECOND_NAME, bindingClass.edSecondName.text.toString())
+            if (bindingClass.imAvatar.isVisible) intent.putExtra(Constance.AVATAR_ID, R.drawable.profil)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
 }
